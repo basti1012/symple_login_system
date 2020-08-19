28e5fa7ad39bab3b89ea399872a68c382a1a328d
basti1012 <48145852+basti1012@users.noreply.github.com>
2020-08-19 02:05:23 +0200

    Update install.php

--- install.php
+++ install.php
@@ -1,13 +1,16 @@
 <?php
 $pfad=$_SERVER['PHP_SELF'];
-$HOST=$_SERVER['HTTP_HOST'];
+ $HOST=$_SERVER['HTTP_HOST'];
 $pfad=explode('/',$pfad);
-$count_ordner=count($pfad)-1;
-for($f=0;$f<$count_ordner;$f++){
-   $HOST.=$pfad[$f].'/';
-}
+ $count_ordner=count($pfad)-1;
+  
+  for($f=0;$f<$count_ordner;$f++){
+$HOST.=$pfad[$f].'/';
+  }
+ 
 $filename3='mysql.php';
 if (file_exists($filename3)) {
+
    echo "Die Setup Datei wurde schon ausgeführt<br>";
    $filename1 = 'setupdatei.php';
    if (file_exists($filename1)) {
@@ -19,15 +22,20 @@ if (file_exists($filename3)) {
    }
    exit;
 } else {
+
 ?>
-<form name="eingabe" action=" setupdatei.php" method="post">   
+
+   <form name="eingabe" action=" setupdatei.php" method="post">   
 <h1>Setup starten</h1>
+
      <label><p>Datenbank Host: </p><input type="text" id="dbhost" name="dbhost" class="input_feld"></label> 
      <label><p>Datenbank Name: </p><input type="text" id="dbname" name="dbname" class="input_feld"></label> 
      <label><p>Datenbank User: </p><input type="text" id="dbuser" name="dbuser" class="input_feld"></label> 
      <label><p>Name der Tabelle: </p><input type="text" id="name_tabelle" name="name_tabelle" value="users" class="input_feld"></label> 
+
+
      <label><p>Passwort: </p><input type="text" id="dbpw" name="dbpw"  class="input_feld"></label> 
-     <label><p>Link zum diesen Ordner:</p><input  type="text" id="pw_link"  class="input_feld input_feld_info" name="pw_link" value="sebastian1012.bplaced.net/Eigen-bau/login-system/">
+     <label><p>Link zum diesen Ordner:</p><input  type="text" id="pw_link"  class="input_feld input_feld_info" name="pw_link" value="<?php echo $HOST; ?>">
   <div class="info">[?]<span class="infotext">
     Oder zum Ordner wo die Passwort wiederherstellungsdatei liegt.Dieser Link wird benötigt um den wiederherstellungs Passwort Links in der Email zu erzeigen
     </span>
@@ -41,11 +49,15 @@ if (file_exists($filename3)) {
   <label>
 <input type="submit" class="buttonstyle" value="Setup Starten">
   </label>
+
 <small>*Die Setupdatein werden nach erfolgreicher Installation vom Server gelöscht.</small>
 <h5>
 Es wird eine Config Datei erstellt.
+ 
 </h5>
  </form>
+
 <?php
 }
+ 
 ?>
