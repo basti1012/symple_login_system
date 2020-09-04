96d1894255df66d35be02afd2c534595dbacaf18
basti1012 <48145852+basti1012@users.noreply.github.com>
2020-09-04 20:22:20 +0200

    Update setupdatei.php

--- setupdatei.php
+++ setupdatei.php
@@ -2,7 +2,8 @@
 <html lang="de">
   <head>
     <meta charset="utf-8">
-    <title>Login</title>
+    <title>Installation</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet">
   </head>
   <body>
