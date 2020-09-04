961755747c6e59d00ef2ef00b8f714889e485d30
basti1012 <48145852+basti1012@users.noreply.github.com>
2020-09-04 20:25:01 +0200

    Update register.php

--- register.php
+++ register.php
@@ -2,20 +2,20 @@
 <html lang="de">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="style.css" rel="stylesheet">
-  </head>
-  <body>
-    <?php
-
+</head>
+<body>
+<?php
     if(isset($_POST["submit"])){
       require("mysql.php");
-      $stmt = $mysql->prepare("SELECT * FROM `$tabelle` WHERE user = :user"); //Username überprüfen
+      $stmt = $mysql->prepare("SELECT `user` FROM `$tabelle` WHERE user = :user"); //Username überprüfen
       $stmt->bindParam(":user", $_POST["username"]);
       $stmt->execute();
       $count = $stmt->rowCount();
       if($count == 0){
-        $stmt = $mysql->prepare("SELECT * FROM `$tabelle` WHERE email = :email");
+        $stmt = $mysql->prepare("SELECT `email` FROM `$tabelle` WHERE email = :email");
         $stmt->bindParam(":email", $_POST["email"]);
         $stmt->execute();
         $count = $stmt->rowCount();
@@ -38,15 +38,18 @@
         echo "<div class='error'>Der Username ist bereits vergeben</div>";
       }
     }
-     ?>
-         <form class="create" action="register.php" method="post">
-    <h1>Account erstellen</h1>
-
-      <input  class="input_feld input_feld_info" type="text" name="username" placeholder="Username" required><br>
-      <input  class="input_feld input_feld_info" type="text" name="email" placeholder="Email" required><br>
-      <input  class="input_feld input_feld_info" type="password" name="pw" placeholder="Passwort" required><br>
-      <input  class="input_feld input_feld_info" type="password" name="pw2" placeholder="Passwort wiederholen" required><br>
-      <button  class="input_feld erstelle" type="submit" name="submit">Erstellen</button>
+?>
+    <form class="create" action="register.php" method="post">
+          <h1>Account erstellen</h1>
+          <label>Username *</label>
+          <input  class="input_feld input_feld_info" type="text" name="username" placeholder="Username" required><br>
+          <label>E-mail *</label>
+          <input  class="input_feld input_feld_info" type="text" name="email" placeholder="Email" required><br>
+          <label>Passord *</label>
+          <input  class="input_feld input_feld_info" type="password" name="pw" placeholder="Passwort" required><br>
+          <label>Password wiederholen</label>
+          <input  class="input_feld input_feld_info" type="password" name="pw2" placeholder="Passwort wiederholen" required><br>
+          <button  class="input_feld erstelle" type="submit" name="submit">Erstellen</button>
     </form>
     <br>
     <a href="index.php">Hast du bereits einen Account?</a>
