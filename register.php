1999530520e623b4da97ff9753b6793131f88178
basti1012 <48145852+basti1012@users.noreply.github.com>
2020-09-04 20:28:14 +0200

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Registrierung</title>
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
